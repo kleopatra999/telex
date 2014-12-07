08b05cf97c0d0cd66dd97dadba98e2116c6689d8
Pedro Belo <pedro@heroku.com>
2014-12-07 11:52:31 -0800

    extract actions for better readability

--- lib/endpoints/producer_api/messages.rb
+++ lib/endpoints/producer_api/messages.rb
@@ -10,40 +10,12 @@ module Endpoints
           target.text :type
           target.text :id
         end
-        link.action do
-          begin
-            message = Mediators::Messages::Creator.run(
-              producer: current_producer,
-              title:       data['title'],
-              body:        data['body'],
-              target_type: data['target'] && data['target']['type'],
-              target_id:   data['target'] && data['target']['id']
-            )
-
-            status 201
-            MultiJson.encode({id: message.id})
-          rescue => e
-            raise Pliny::Errors::UnprocessableEntity
-          end
-        end
+        link.action { create_message }
       end
 
       res.link(:post, "/:message_id/followups") do |link|
         link.property.text :body
-        link.action do
-          begin
-            message = Message[id: params['message_id'], producer_id: current_producer.id]
-            followup = Mediators::Followups::Creator.run(
-              message: message,
-              body:    data['body']
-            )
-
-           status 201
-           MultiJson.encode({id: followup.id})
-          rescue
-            raise Pliny::Errors::UnprocessableEntity
-          end
-        end
+        link.action { create_followup }
       end
     end
 
@@ -53,5 +25,32 @@ module Endpoints
       Pliny::RequestStore.store.fetch(:current_producer)
     end
 
+    def create_message
+      message = Mediators::Messages::Creator.run(
+        producer: current_producer,
+        title:       data['title'],
+        body:        data['body'],
+        target_type: data['target'] && data['target']['type'],
+        target_id:   data['target'] && data['target']['id']
+      )
+
+      status 201
+      MultiJson.encode({id: message.id})
+    rescue => e
+      raise Pliny::Errors::UnprocessableEntity
+    end
+
+    def create_followup
+      message = Message[id: params['message_id'], producer_id: current_producer.id]
+      followup = Mediators::Followups::Creator.run(
+        message: message,
+        body:    data['body']
+      )
+
+     status 201
+     MultiJson.encode({id: followup.id})
+    rescue
+      raise Pliny::Errors::UnprocessableEntity
+    end
   end
 end
